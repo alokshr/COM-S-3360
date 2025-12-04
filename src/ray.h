137667152d30fb8610775276ce322059beaa09c7
Alok Shrestha <aloks@iastate.edu>
2025-12-04 14:13:06 -0600

    Added comments to ray.h

--- src/ray.h
+++ src/ray.h
@@ -7,12 +7,36 @@
  */
 class ray {
     public:
+
+        /**
+         * Creates a default ray
+         */
         ray() {};
+
+        /**
+         * Creates a ray from an origin point and a direction
+         * @param origin origin point of ray
+         * @param direction direction of ray 
+         */
         ray(const vec3& origin, const vec3& direction): orig(origin), dir(direction) {};
 
+        /**
+         * Returns the origin point of this ray
+         * @return origin point
+         */
         const vec3& origin() const  { return orig; }
+
+        /**
+         * Return the direction of this ray
+         * @return direction
+         */
         const vec3& direction() const { return dir; }
 
+        /**
+         * Returns the point along the ray
+         * @param t time/percentage along the ray
+         * @return position at given t
+         */
         vec3 at(double t) const {
             return orig + t*dir;
         }
